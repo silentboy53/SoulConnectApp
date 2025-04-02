8890562292a69936c4cbe1dc368efbe769141701
silentboy53 <sanketsawant53@gmail.com>
2025-04-02 18:00:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+    document.getElementById('lostLovedOne').addEventListener('click', function() {
+        document.getElementById('message').innerText = "I still hear you in the silence...";
+    });
+
+    document.getElementById('futureSelf').addEventListener('click', function() {
+        document.getElementById('message').innerText = "You will find peace in the chaos...";
+    });
+
+    document.getElementById('pastLife').addEventListener('click', function() {
+        document.getElementById('message').innerText = "Once, you were a warrior on the shores of a forgotten land...";
+    });
+    
